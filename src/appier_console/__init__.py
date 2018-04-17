0ec860a116f7844dc9d7be148546110e54d01417
joamag <joamag@hive.pt>
2018-04-17 16:04:41 +0100

    new symbols export

--- src/appier_console/__init__.py
+++ src/appier_console/__init__.py
@@ -38,6 +38,9 @@ from . import base
 from . import http
 from . import util
 
-from .base import LoaderThread, ctx_loader
+from .base import COLOR_RESET, COLOR_WHITE, COLOR_BLACK, COLOR_BLUE, COLOR_LIGHT_BLUE, COLOR_GREEN,\
+    COLOR_LIGHT_GREEN, COLOR_CYAN, COLOR_LIGHT_CYAN, COLOR_RED, COLOR_LIGHT_RED, COLOR_PURPLE,\
+    COLOR_LIGHT_PURPLE, COLOR_BROWN, COLOR_YELLOW, COLOR_GRAY, COLOR_LIGHT_GRAY, CLEAR_LINE, COLORS,\
+    LoaderThread, ctx_loader
 from .http import ctx_http_callbacks
 from .util import is_tty, is_color
