fb0081e16826b47359eb0de2cd822712800f0447
joamag <joamag@hive.pt>
2018-04-17 16:13:29 +0100

    new color support

--- src/appier_console/base.py
+++ src/appier_console/base.py
@@ -260,6 +260,7 @@ def ctx_loader(*args, **kwargs):
         thread.join()
 
 def colored(value, color = COLOR_RED):
+    color = COLORS.get(color, color)
     return color + value + COLOR_RESET
 
 if __name__ == "__main__":
