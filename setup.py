47619e7b5c8f5e017b7e48a5548a6c2279f74dd1
joamag <joamag@gmail.com>
2019-06-20 01:17:29 +0100

    New version 0.2.3

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ import setuptools
 
 setuptools.setup(
     name = "appier_console",
-    version = "0.2.2",
+    version = "0.2.3",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Appier Console",
